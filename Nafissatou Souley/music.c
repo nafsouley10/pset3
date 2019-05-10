2dc384d1dbbaf30005df3d176f3fd2250b4e5ede
nafsouley10 <47788474+nafsouley10@users.noreply.github.com>
2019-05-10 12:55:03 +0100

    abil27
    assessment

---
+++ Nafissatou Souley/music.c
@@ -0,0 +1,72 @@
+// Helper functions for music
+#include <cs50.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "helpers.h"
+{
+  int numerator = fraction[0] - '0';
+int denominator = fraction[2] - '0';
+}
+{
+int octave = note[strlen(note) - 1] - '0';
+double freq = 0.0;
+if (note[0] == 'A') 
+{ 
+freq = 440;
+}
+else if (note[0] == 'B') { freq = 440.0 * (pow(2.0, (2.0 / 12.0)));
+
+}
+else if (note[0] == 'C')
+{
+    freq = 440.0 / (pow(2.0, (9.0 / 12.0)));
+}
+else if (note[0] == 'D')
+{
+    freq = 440.0 / (pow(2.0, (7.0 / 12.0)));
+}
+else if (note[0] == 'E')
+{
+    freq = 440.0 / (pow(2.0, (5.0 / 12.0)));
+    }
+    else if (note[0] == 'F')
+    {
+        freq = 440.0 / (pow(2.0, (4.0 / 12.0)));
+        }
+        else if (note[0] == 'G')
+        {
+            freq = 440.0 / (pow(2.0, (2.0 / 12.0)));
+            }
+             if (octave > 4)
+            {
+                for (int i = 0; i < octave - 4; i++)
+                { freq *= 2.0; }
+                }
+                else if (octave < 4)
+                {
+                    for (int i = 0; i < 4 - octave; i++)
+                    {
+                        freq /= 2.0;
+                       }
+                    }
+                    if (note[1] == 'b')
+                    {
+                        freq /= (pow(2.0, (1.0 / 12.0)));
+                        }
+                        else if (note[1] == '#')
+                        {
+                            freq *= (pow(2.0, (1.0 / 12.0)));
+                            }
+                            int result = round(freq);
+                            return result;
+                            }
+       
+                            {
+                       if (s[0] == '\0')
+
+                            {
+                                return true;
+                            }
+                            else
+{ return false; }}
